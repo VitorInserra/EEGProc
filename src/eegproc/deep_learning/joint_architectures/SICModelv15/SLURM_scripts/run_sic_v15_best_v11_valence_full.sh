60495f865b78c6df6194703fa0e28c87fc998a72
VitorInserra <inserra.vitor@gmail.com>
2026-09-07 13:50:38 -0400

    Add parallel optimization

--- src/eegproc/deep_learning/joint_architectures/SICModelv15/SLURM_scripts/run_sic_v15_best_v11_valence_full.sh
+++ src/eegproc/deep_learning/joint_architectures/SICModelv15/SLURM_scripts/run_sic_v15_best_v11_valence_full.sh
@@ -176,14 +176,14 @@ print(json.dumps({
 
     "use_subject_adversarial": True,
     "subject_adversarial_weight": 0.6,
-    "subject_loss_weight": 1.0,
+    "subject_loss_weight": 0.2,
     "subject_hidden_units": 64,
     "subject_dropout": 0.0,
 
     "use_gcn_gru_branch": True,
     "use_bilstm_branch": True,
     "use_decoder": True,
-    "reconstruction_loss_weight": 0.1,
+    "reconstruction_loss_weight": 0.4,
     "decoder_dropout": 0.1,
     "joint_reconstruction_auxiliary_weight": 0.25,
     "joint_reconstruction_initial_alpha": 0.5,
